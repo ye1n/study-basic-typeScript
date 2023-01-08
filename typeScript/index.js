88299a07fe10a2e56dc3d31d7ee0f79f01e9ef09
우예인 <uyeinyein@gmail.com>
2023-01-08 20:38:57 +0900

    타입스크립트 기본 타입 정리

--- typeScript/index.js
+++ typeScript/index.js
@@ -1,7 +1,10 @@
-//변수 생성시 타입 지정
+//터미널-> tsc -w (js파일로 변경)
+//변수 생성시 타입 지정(기본적으로 타입 자동 부여 됨)
 var v1 = ['kim', 'kang'];
 var v2 = 12;
 var v3 = { age: 20 };
+//특정 속성이 선택사항일때 물음표 기입
+var v4 = ['kim', 'kang'];
 //타입 선언
 function func(x) {
     return x * 2;
@@ -14,3 +17,19 @@ var User = /** @class */ (function () {
     }
     return User;
 }());
+// Q1. 나의 이름, 나이, 출생지역을 변수로 저장
+var myName = '우예인';
+var myAge = 18;
+var myContry = '서울';
+var myFavorite = { song: '잔나비', singer: '가을밤에 든 생각' };
+// Q3. 아래 자료를 타입지정
+// let project = {
+//     member: ['kim', 'park'],
+//     days: 30,
+//     started: true,
+// }
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
--- typeScript/index.ts
+++ typeScript/index.ts
@@ -1,8 +1,10 @@
-//변수 생성시 타입 지정
-let v1: string[] = ['kim', 'kang'];
+//터미널-> tsc -w (js파일로 변경)
+//변수 생성시 타입 지정(기본적으로 타입 자동 부여 됨)
+let v1: string[] | number[] = ['kim', 'kang'];
 let v2: Type1 = 12;
 let v3: { name?: string, age: number } = { age: 20 };
 //특정 속성이 선택사항일때 물음표 기입
+let v4: [] | {} = ['kim', 'kang'];
 type Type1 = string | number;
 //타입 선언
 
@@ -26,4 +28,31 @@ class User {
     constructor(name: string) {
         this.name = name;
     }
+}
+
+// Q1. 나의 이름, 나이, 출생지역을 변수로 저장
+let myName: string = '우예인'
+let myAge: number = 18
+let myContry: string = '서울'
+
+// Q2. 가장 좋아하는 곡과 가수이름을 변수에 object 자료형으로 저장
+type myType = {
+    [key: string]: string,
+}
+let myFavorite: myType = { song: '잔나비', singer: '가을밤에 든 생각' }
+
+// Q3. 아래 자료를 타입지정
+// let project = {
+//     member: ['kim', 'park'],
+//     days: 30,
+//     started: true,
+// }
+let project: {
+    member: string[],
+    days: number,
+    started: boolean
+} = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
 }
